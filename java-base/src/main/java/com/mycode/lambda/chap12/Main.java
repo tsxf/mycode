7e8f325e83bda6787aa13adc2a67802cdb12f80b
jiangfu <bohunwuya@outlook.com>
2019-04-02 10:21:26 +0800

    修改日期使用说明

--- java-base/src/main/java/com/mycode/lambda/chap12/Main.java
+++ java-base/src/main/java/com/mycode/lambda/chap12/Main.java
@@ -11,6 +11,7 @@ import java.time.temporal.ChronoUnit;
 public class Main {
     public static void main(String [] args){
         LocalDate date1 = LocalDate.of(2014, 3, 18);
+        //有两种修改方式：1.直接修改为目标值（绝对），2. 在现在的值基础上做修改（相对）
         LocalDate plus = date1.plus(6, ChronoUnit.MONTHS);
         System.out.println(plus);
         LocalDate localDate = date1.plusWeeks(1);
@@ -21,7 +22,9 @@ public class Main {
         System.out.println(localDate2);
         LocalDate localDate3 = date1.with(ChronoField.DAY_OF_MONTH, 9);
         localDate3 = localDate3.plusYears(6).minusDays(3);
-        localDate3.withYear(11);
+        LocalDate temp = localDate3.withYear(2011);
+        //不可变对象，每次修改后都会生成一个新的对象
+        System.out.println(temp);
         System.out.println(localDate3);
 
 
